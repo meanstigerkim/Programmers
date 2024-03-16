778f2d13953e22b4c1bb83a0130ddf5303da1f11
도훈 김 <meanstigerkim@gmail.com>
2024-03-16 23:06:40 +0900

    Day05

---
+++ Programmers/Day05.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class Day05
+    {
+        public class Solution
+        {
+            public int solution(int n)
+            {
+
+                int answer = 0;
+                answer = Fibo(n);
+
+                return answer;
+            }
+
+            private static int Fibo(int n)
+            {
+
+                if (n < 2)
+                {
+                    return n;
+                }
+                int k = 0;
+
+                k = (Fibo(n - 2) + Fibo(n - 1)) % 1234567;
+
+                return k;
+            }
+        }
+    }
+}
