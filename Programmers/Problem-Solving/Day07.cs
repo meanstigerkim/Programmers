19deaf1666fb764128482eadad32843ca986b1bf
도훈 김 <meanstigerkim@gmail.com>
2024-03-26 23:43:32 +0900

    add

--- Programmers/Problem-Solving/Day07.cs
+++ Programmers/Problem-Solving/Day07.cs
@@ -8,5 +8,23 @@ namespace Programmers.Problem_Solving
 {
     internal class Day07
     {
+
+        public static void Main(string[] args)
+        {
+            //자 릿수 더하기
+
+            int answer = 0;
+
+
+            char[] chars = n.ToString().ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                answer += int.Parse(chars[i].ToString());
+            }
+
+           
+
+        }
     }
 }
---
+++ Programmers/Theory/SildingWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers.Theory
+{
+    public class SildingWindow
+    {
+        //슬라이딩 윈도우 알고리즘은 2개의 포인터로 범위를 지정한 다음 범위(Window)를
+        //유지한채로 이동(Silding)하여 문제를 해결합니다.
+
+         
+
+    }
+}
