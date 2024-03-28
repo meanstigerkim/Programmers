3c993daa4e6027956c159b10a4f151a298d4a18a
도훈 김 <meanstigerkim@gmail.com>
2024-03-28 23:13:47 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/Problem-Solving/Day09.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers.Problem_Solving
+{
+    internal class Day09
+    {
+        public class Solution
+        {
+            public long[] solution(long x, long n)
+            {
+                //x만큼 간격이 있는 n개의 숫자
+
+                //문제 설명
+                //함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+                //제한 조건
+                //x는 - 10000000 이상, 10000000 이하인 정수입니다.
+                //n은 1000 이하인 자연수입니다.
+
+                long[] answer = new long[n];
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    if (i == 0)
+                    {
+                        answer[i] = x;
+                    }
+                    else
+                    {
+                        answer[i] = (x * (i + 1));
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
+}
